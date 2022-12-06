9f447f2f92bbfb97a89273e25d9383cd853eb844
Terry Boufis <terryboufis@gmail.com>
2022-12-05 20:17:06 -0600

    added model.js files

---
+++ db/schema.sql
@@ -0,0 +1,7 @@
+-- DROP DATABASE
+DROP DATABASE IF EXISTS subscription_db;
+
+-- CREATE DATABASE
+CREATE DATABASE subscription_db;
+
+USE subscription_db;
---
+++ models/Subscription.js
@@ -0,0 +1,45 @@
+// import important parts of sequelize library
+const { Model, DataTypes } = require('sequelize');
+// import our database connection from config.js
+const sequelize = require('../config/connection');
+
+// Initialize Product model (table) by extending off Sequelize's Model class
+class Subscription extends Model {}
+
+// set up fields and rules for Product model
+Subscription.init(
+    {
+      // define columns
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      subscription_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      price: {
+        type: DataTypes.DECIMAL,
+        allowNull: false,
+      },
+      user_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'category',
+          key: 'id',
+          unique: false
+        }
+      },
+    },
+    {
+      sequelize,
+      timestamps: false,
+      freezeTableName: true,
+      underscored: true,
+      modelName: 'subscription',
+    }
+  );
+  
+  module.exports = Subscription;
