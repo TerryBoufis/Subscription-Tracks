2d29c6454326b9510da131c6eef22cc18cb511c0
jwm <jwm3926@gmail.com>
2022-12-06 19:04:31 -0500

    started work on profile.js and fixed pai to api in login.js

---
+++ public/profile.js
@@ -0,0 +1,10 @@
+const newSubHandler = async (event) => {
+    event.preventDefault();
+
+    const name = document.querySelector('#subscription-name').value.trim();
+    const price = document.querySelector('#price').value.trim();
+
+    if (name && price) {
+        
+    }
+}
