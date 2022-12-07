9263b4f63aefb77156d955a715e51e714809b511
Terry Boufis <terryboufis@gmail.com>
2022-12-06 19:14:47 -0600

    Added more to the connection folder

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -30,3 +30,47 @@ router.get('/', async (req, res) => {
     // finds a single user by its `id`
     // includes their associated subscription data
   });
+
+  router.post("/", async (req, res) => {
+    try {
+      const userData = await User.create(req.body);
+      res.status(200).json(userData);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
+
+  router.put("/:id", async (req, res) => {
+    try {
+      const userData = await User.update(req.body, {
+        where: {
+          id: req.params.id,
+        },
+      });
+      res.status(200).json(userData);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
+
+  router.delete('/:id', async (req, res) => {
+    try {
+      const userData = await User.destroy({
+        where: {
+          id: req.params.id
+        }
+      });
+  
+      if (!userData) {
+        res.status(404).json({ message: 'No user found with this id!' });
+        return;
+      }
+  
+      res.status(200).json(userData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+    // deletes a user by its `id` value
+  
+  module.exports = router;
