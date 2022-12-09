158ff3d5e4e0c6fa497cd3be5229e450a165672b
jwm <jwm3926@gmail.com>
2022-12-08 21:22:35 -0500

    added functinality to save log in, not currently working though

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -34,7 +34,12 @@ router.get('/', async (req, res) => {
   router.post("/", async (req, res) => {
     try {
       const userData = await User.create(req.body);
-      res.status(200).json(userData);
+      req.session.save(() => {
+        req.session.user_id = userData.id;
+        req.session.loggedin = true;
+
+        res.status(200).json(userData);
+      })
     } catch (err) {
       res.status(400).json(err);
     }
@@ -72,5 +77,45 @@ router.get('/', async (req, res) => {
     }
   });
     // deletes a user by its `id` value
+
+  router.post('/login', async (req, res) => {
+    try {
+      const userData = await User.findOne({ where: { email: req.body.email } });
+
+      if (!userData) {
+        res
+          .status(400)
+          .json({ message: 'Incorrect email or password, please try again.' });
+      }
+
+      const validPassword = await userData.checkPassword(req.body.password);
+
+      if (!validPassword) {
+        res
+          .status(400)
+          .json({ message: 'Incorrect password, please try again' });
+        return;
+      }
+
+      req.session.save(() => {
+        req.session.user_id = userData.id;
+        req.session.logged_in = true;
+
+        res.json({ user: userData, message: 'You are now logged in!' });
+      });
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  })
+
+  router.post('/logout', (req, res) => {
+    if(req.session.logged_in) {
+      req.session.destroy(() => {
+        res.status(204).end();
+      });
+    } else {
+      res.status(404).end();
+    }
+  })
   
   module.exports = router;
